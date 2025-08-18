96ffb64fa449c7c7169462ecd3a4c056f84b39c5
Nguyen Van Viet <nguyenviet@VNPC100.vsvn.com>
2025-08-18 17:56:55 +0700

    add code

--- src/features/Leave/ViewOnlyLeaveRq.tsx
+++ src/features/Leave/ViewOnlyLeaveRq.tsx
@@ -6,6 +6,7 @@ import leaveRequestApi from "@/api/leaveRequestApi"
 import { formatDate } from "@/lib/time"
 
 const ViewOnlyLeaveRq = () => {
+    const { t:tApproval } = useTranslation('pendingApproval')
     const lang = useTranslation().i18n.language.split('-')[0]
     const { id } = useParams<{ id: string }>()
 
@@ -21,40 +22,44 @@ const ViewOnlyLeaveRq = () => {
     return (
         <div className="p-4 pl-1 pt-0 space-y-4">
             <div className="flex flex-wrap justify-between items-center gap-y-2 gap-x-4 mb-3">
-                <h3 className="font-bold text-xl md:text-2xl m-0 pb-2">Chi tiết đơn nghỉ phép</h3>
+                <h3 className="font-bold text-xl md:text-2xl m-0 pb-2">{tApproval('detail_approval_leave_request.title')}</h3>
             </div>
             <div className="text-left mb-6 border-t border-dashed border-gray-400 pt-2">
                 <p className="my-2 text-xl">
-                    <strong className="text-blue-600">Mã đơn:</strong> <span className="font-bold text-gray-600">{leaveRequest?.code}</span>
+                    <strong className="text-blue-600">{tApproval('detail_approval_leave_request.code')}:</strong> <span className="font-bold text-gray-600">{leaveRequest?.code}</span>
                 </p>
                 <p className="my-2">
-                    <strong>Mã nhân viên:</strong> {leaveRequest?.userCodeRequestor}
+                    <strong>{tApproval('detail_approval_leave_request.user_code')}:</strong> {leaveRequest?.userCodeRequestor}
                 </p>
                 <p className="my-2">
-                    <strong>Người gửi:</strong> {leaveRequest?.userNameRequestor}
-                </p>
-
-                <p className="my-2">
-                    <strong>Phòng ban:</strong> {leaveRequest?.orgUnit.name}
-                </p>
-                <p className="my-2">
-                    <strong>Chức vụ:</strong> {leaveRequest?.position}
+                    <strong>{tApproval('detail_approval_leave_request.user_requestor')}:</strong> {leaveRequest?.userNameRequestor}
                 </p>
 
                 <p className="my-2">
-                    <strong>Ngày bắt đầu:</strong> {formatDate(leaveRequest?.fromDate, 'yyyy-MM-dd HH:mm:ss')}
+                    <strong>{tApproval('detail_approval_leave_request.department')}:</strong> {leaveRequest?.orgUnit.name}
+                </p>
+                <p className="my-2">
+                    <strong>{tApproval('detail_approval_leave_request.position')}:</strong> {leaveRequest?.position}
                 </p>
 
                 <p className="my-2">
-                    <strong>Ngày kết thúc:</strong> {formatDate(leaveRequest?.toDate, 'yyyy-MM-dd HH:mm:ss')}
+                    <strong>{tApproval('detail_approval_leave_request.from_date')}:</strong> {formatDate(leaveRequest?.fromDate, 'yyyy-MM-dd HH:mm:ss')}
                 </p>
 
                 <p className="my-2">
-                    <strong>Thời gian nghỉ:</strong> {lang == 'vi' ? leaveRequest?.timeLeave.name : leaveRequest?.timeLeave.nameE}
+                    <strong>{tApproval('detail_approval_leave_request.to_date')}:</strong> {formatDate(leaveRequest?.toDate, 'yyyy-MM-dd HH:mm:ss')}
                 </p>
 
                 <p className="my-2">
-                    <strong>Lý do:</strong> {leaveRequest?.reason}
+                    <strong>{tApproval('detail_approval_leave_request.type_leave')}</strong> {lang == 'vi' ? leaveRequest?.typeLeave.name : leaveRequest?.typeLeave.nameE}
+                </p>
+
+                <p className="my-2">
+                    <strong>{tApproval('detail_approval_leave_request.time_leave')}:</strong> {lang == 'vi' ? leaveRequest?.timeLeave.name : leaveRequest?.timeLeave.nameE}
+                </p>
+
+                <p className="my-2">
+                    <strong>{tApproval('detail_approval_leave_request.reason')}:</strong> {leaveRequest?.reason}
                 </p>
             </div>
 
